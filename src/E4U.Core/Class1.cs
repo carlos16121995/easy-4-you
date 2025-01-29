5b545b64c68ea706b26ddb227465fe0efd6a7583
Carlos Alcântara <carloseduardo161219@gmail.com>
2025-01-29 08:52:15 -0300

    feature: criação do repositório

---
+++ src/E4U.Core/Class1.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2024 Result Consultoria Empresarial®. All rights reserved.
+// Copyright (c) 2024 Startando.com.vc®. All rights reserved.
+// PRIVATE SOURCE. Any kind of unauthorized use is prohibited.
+
+namespace E4U.Core;
+
+public class Class1
+{
+
+}
